384ff8e4a38a633ec44ac99f4660a7d7fe73e2fd
Angus Watson <angus.watson16@gmail.com>
2018-12-06 16:10:24 +0000

    tinkering with index page to solve heroku error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,1 @@
 import "bootstrap";
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
