fb12c36f2928daf18e46c47057578960a254f51d
Angus Watson <angus.watson16@gmail.com>
2018-12-07 12:16:38 +0000

    added a file path to partilces load.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,4 @@
 //= require_tree .
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json');
+particlesJS.load('particles-js', 'assets/particles.json', 'assets/particles.min.js');
